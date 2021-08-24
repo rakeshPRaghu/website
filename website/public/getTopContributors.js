c7795b524e8b3c66fd2fa79fec37e4a84222920b
Rakesh PR <rakesh.pr@mayadata.io>
2021-08-24 17:10:34 +0530

    added option for fetching new contributors
    Signed-off-by: Rakesh PR <rakesh.pr@mayadata.io>

--- website/public/getTopContributors.js
+++ website/public/getTopContributors.js
@@ -13,13 +13,19 @@ const getdatesForContribution = () => {
   };
 };
 
-const reqBodyToFetchContributors = {
+const rawSql = {
+  topContributors:
+    "select\n  row_number() over (order by value desc) as \"Rank\",\n  name,\n  value\nfrom\n  shpr_auth\nwhere\n  series = 'hpr_authall'\n  and period = 'm'",
+  newContributors:
+    "select str, dt from \"snew_contributors_data\" where $__timeFilter(dt) and series = 'ncdall' and period = 'd'",
+};
+
+const reqBodyToFetchContributors = (type) => ({
   queries: [
     {
       refId: "A",
       datasourceId: 1,
-      rawSql:
-        "select\n  row_number() over (order by value desc) as \"Rank\",\n  name,\n  value\nfrom\n  shpr_auth\nwhere\n  series = 'hpr_authall'\n  and period = 'm'",
+      rawSql: type === "topContributors" ? rawSql.topContributors : rawSql.newContributors,
       format: "table",
       intervalMs: 86400000,
       maxDataPoints: 1255,
@@ -35,24 +41,39 @@ const reqBodyToFetchContributors = {
   },
   from: `${getdatesForContribution().priorDateTimestamp}`,
   to: `${getdatesForContribution().todayTimestamp}`,
-};
+});
 
-const settings = {
+const settings = (type) => ({
   url: "https://openebs.devstats.cncf.io/api/ds/query",
   method: "POST",
   json: true,
   headers: {
     "Content-Type": "application/json",
   },
-  body: reqBodyToFetchContributors,
-};
+  body: reqBodyToFetchContributors(type),
+});
 
-function fetchContributors() {
-  request.post(settings, function (error, response, body) {
-    if(error) { console.error(error); return;}
+const fetchContributors = async () =>  {
+  const topContributors = await request.post(settings("topContributors"), function (error, response, body) {
+    if (error) {
+      console.error(error);
+      return;
+    }
     const data = JSON.stringify(body?.results?.A?.frames[0]?.data?.values[1]);
     data && fs.writeFileSync("src/resources/topContributors.json", data);
+    return data;
+  });
+
+  topContributors && await request.post(settings("newContributors"), function (error, response, body) {
+    if (error) {
+      console.error(error);
+      return;
+    }
+    const data = JSON.stringify(
+      body?.results?.A?.frames[0]?.data?.values[0]?.reverse()
+    );
+    data && fs.writeFileSync("src/resources/newContributors.json", data);
   });
 }
 
-fetchContributors();
+fetchContributors()
